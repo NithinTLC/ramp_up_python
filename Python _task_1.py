49daff9907b927d1ffab14360f8283ceb0bb511d
Nithin <Nithinterralogic@gmail.com>
2023-08-22 14:10:44 +0530

    Fille added

---
+++ Python _task_1.py
@@ -0,0 +1,69 @@
+#Userin = input("Enter the statement: ")
+Userin ="Testing the program for character and word manipulation. This is a test statement for testing purposes"
+print("The Total number of characters in the statement: ",len(Userin))
+Userin=Userin.lower()
+lC = Userin.lower()
+def t1():
+    dup=""
+    dupchar=""
+    for i in lC:
+        if i not in dup:
+                dup=dup+i
+        else:
+            if i not in dupchar:
+                dupchar+=i
+    count_dup=len(lC)-len(dup)
+    print(f"Duplicates in the Statement are: {dupchar}")
+    print(f"The Total number of duplicates in the statement:{count_dup}")
+
+#task2
+def t2():
+    # Userin = input("enter the characters")
+    words = Userin.split()
+    Words_count = len(words)
+    print(f"No of words in the given Statement is: {Words_count}")
+
+#task3
+def t3():
+    words = Userin.split()
+    Dup = []
+    Dup_words = []
+    for i in words:
+        if i not in Dup:
+            Dup.append(i)
+        else:
+            if i not in Dup_words:
+                Dup_words.append(i)
+    #print(Dup_words)
+    Words_count = len(Dup_words)
+    print(f"No of words in the given input is: {Words_count}")
+#task4
+def t4():
+    reversed_char = Userin[::-1]
+    print(f"The Statement after reversing the characters: {reversed_char}")
+#task5
+def t5():
+    Words = Userin.split()
+    revers = Words[::-1]
+    reverss = " ".join(revers)
+    # print(Words)
+    print(f"The statement after the reversing the order of the words: {reverss}")
+#task6
+def t6():
+    dup = ""
+    dupchar = ""
+    for i in Userin:
+        if i not in dup:
+            dup = dup + i
+        else:
+            if i not in dupchar:
+                dupchar += i
+    count_dup = len(lC) - len(dup)
+    #print(dupchar)
+    print(f"The latest statment after remove the the duplicates char from statement: {dup}")
+t1()
+t2()
+t3()
+t4()
+t5()
+t6()
